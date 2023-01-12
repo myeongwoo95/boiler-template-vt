1638530c08270ffde21603676baf7fb91ef94619
walle <myeongwoo95@naver.com>
2023-01-12 18:15:00 +0900

    :>

---
+++ src/main/java/com/springboot/template/application/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.springboot.template.application.controller;
+
+import com.springboot.template.application.request.UserSignInRequest;
+import com.springboot.template.application.request.UserSignupRequest;
+import com.springboot.template.application.response.UserSignInResponse;
+import com.springboot.template.domain.service.AuthService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @ApiOperation(value = "회원가입")
+    @PostMapping("/v1/api/auth/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(
+            @RequestBody UserSignupRequest userSignupRequest
+    ) {
+        authService.signup(userSignupRequest);
+    }
+
+    @ApiOperation(value = "로그인")
+    @PostMapping("/v1/api/auth/signin")
+    @ResponseStatus(HttpStatus.OK)
+    public UserSignInResponse signIn(
+            @RequestBody UserSignInRequest userSignInRequest
+    ) {
+        String accessToken = authService.signIn(userSignInRequest);
+        return new UserSignInResponse(accessToken);
+    }
+}
